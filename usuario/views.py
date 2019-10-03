d9334dd33fd4a6e041b944ef3b679f00691c3b9f
Akijunior <suitsu19@gmail.com>
2019-10-03 11:12:05 -0300

    Inicia processo de consumo da api pela aplicação.

--- usuario/views.py
+++ usuario/views.py
@@ -1,14 +1,16 @@
+import json
+
+import requests
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
-
 from django.core.mail import send_mail
-from django.conf import settings
-
-# Create your views here.
+from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import CreateView
 
+from propriedade.models import Propriedade
 from usuario.forms import InstituicaoSignUpForm, SolicitarNovaSenhaForm
 from usuario.models import Instituicao
 from utils.criarSenha import criarSenhaAleatoria
@@ -16,15 +18,39 @@ from utils.criarSenha import criarSenhaAleatoria
 
 @login_required
 def index(request):
-    return render(request, 'index.html')
+    response = requests.get('http://teste.aquabit.com.br/api/v1/propriedades/')
+
+    dicio_json = response.json()
+    dicio_string = json.dumps(dicio_json)
+    dicio_python = json.loads(dicio_string)
+
+    dicio_user = dicio_python["results"]
+    lista = []
+
+    for i in dicio_user:
+        for key in i.keys():
+            if key in ['id', 'nome', 'estado', 'cnpj',
+                           'inscricao_estadual', 'cidade', 'email',
+                           'telefone']:
+                lista.append(key)
+        break
+        # print(i["cidade"])
+
+    return render(request, 'index.html', {
+        'usuarios': dicio_user,
+        'lista': lista,
+        'usuario': request.user
+    })
+
 
 def email(request):
     subject = 'Thank you for registering to our site'
     message = ' it  means a world to us '
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['luijunior96@gmail.com',]
-    send_mail( subject, message, email_from, recipient_list, fail_silently=False, )
+    recipient_list = ['luijunior96@gmail.com', ]
+    send_mail(subject, message, email_from, recipient_list, fail_silently=False, )
     return redirect('index')
+
 
 def solicitarNovaSenha(request):
     form = SolicitarNovaSenhaForm(request.POST or None)
@@ -38,10 +64,10 @@ def solicitarNovaSenha(request):
             subject = 'Solicitação de Nova Senha'
             message = ' Olá. Você solicitou uma troca de senha e para tanto foi gerado abaixo sua nova senha de ' \
                       'acesso ao sistema.\nNova senha: %s\nObrigado pela atenção. Acesse o link ' \
-                                                                     'http://127.0.0.1:8000/ para se conectar conosco. \n' \
-                                                                     'Volte sempre. ' % novaSenha
+                      'http://127.0.0.1:8000/ para se conectar conosco. \n' \
+                      'Volte sempre. ' % novaSenha
             email_from = settings.EMAIL_HOST_USER
-            recipient_list = [form.cleaned_data['email'],]
+            recipient_list = [form.cleaned_data['email'], ]
             send_mail(subject, message, email_from, recipient_list, fail_silently=False)
 
             usuario.set_password(novaSenha)
@@ -53,7 +79,6 @@ def solicitarNovaSenha(request):
         else:
             messages.error(request, 'Não existe nenhum usuário cadastrado com esse e-mail')
 
-
     return render(request, 'registration/newPasswordResetForm.html', {'form': form})
 
 
@@ -64,5 +89,20 @@ class InstituicaoSignUpView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
+
+        response = requests.get('http://teste.aquabit.com.br/api/v1/propriedades/')
+
+        dicio_json = response.json()
+        dicio_string = json.dumps(dicio_json)
+        dicio_python = json.loads(dicio_string)
+
+        dicio_user = dicio_python["results"]
+
+        # for i in dicio_user:
+        #     if form.cleaned_data['cidade'] == i['cidade'] and form.cleaned_data['estado'] == i['estado']:
+        #         if not Propriedade.objects.filter(idApi=i["id"]).exists():
+        #             Propriedade.objects.create(idApi=i['id'], nome=i['nome'], nomeProprietario=i['usuarios'][0]['nome'],
+        #                                        qtdAtual=)
+
         login(self.request, user)
         return redirect('index')
